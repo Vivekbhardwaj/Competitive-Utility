1c8e3ee8aae120d3b68b93a9e341cacd4803a2d6
Vivek Bhardwaj <vivekbhardwaj0067@gmail.com>
2020-09-24 14:03:16 +0530

    Update LazySegment.cpp

--- LazySegment.cpp
+++ LazySegment.cpp
@@ -1,131 +1,86 @@
-//For sum query while adding the value of lazy[] to the node we add it after multiplting 
-//it with the interval length of that node of tree
-
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long 
+#define int long long
 
-const int MAXN=4e5+5;
-int seg_tree_min_range_query[4*MAXN];
-int lazy_min_range_query[4*MAXN];
-int n;
+const int inf = 1e18;
+const int MOD = 1e9+7;
 
-void propogate_to_child_min_query(int curr,int value){
-    seg_tree_min_range_query[curr]+=value;
-    lazy_min_range_query[2*curr]+=value;
-    lazy_min_range_query[2*curr+1]+=value;
-    lazy_min_range_query[curr]=0;
-}
+class SegTree{
 
-void update_min(int ul,int ur,int value,int curr=1,int l=0,int r=n-1){
+    private : 
+    vector<pair<int,int>> seg;
+    vector<int> lazy;
+    int n;
 
-    if(r<l) return ;
-    if(ul>r||ur<l) return ;
+    public : 
 
-    if(lazy_min_range_query[curr]){
-        propogate_to_child_min_query(curr,lazy_min_range_query[curr]);
+    SegTree(int n,vector<int> &v){
+        this->n = n;
+        this->seg.resize(8*n);
+        this->lazy.resize(8*n);
+        build(0,n-1,1,v);
+
     }
 
-    if(l>=ul&&r<=ur){
-        seg_tree_min_range_query[curr]+=value;
-        lazy_min_range_query[2*curr]+=value;
-        lazy_min_range_query[2*curr+1]+=value;
+    inline pair<int,int> combine(pair<int,int> l,pair<int,int> r){
+        return l.first < r.first ?  l : r;
+    }
+
+    void build(int l,int r,int curr,vector<int> &v){
+        if(r<l) return;
+        if(l == r){
+            seg[curr].first = v[l];
+            seg[curr].second = l;
+            return;
+        }
+        int mid = (l+r)/2;
+        build(l,mid,2*curr,v);
+        build(mid+1,r,2*curr+1,v);
+        seg[curr] = combine(seg[2*curr],seg[2*curr + 1]);
+    }
+
+    void propogate_down(int curr){
+        seg[curr].first+=lazy[curr];
+        lazy[2*curr]+=lazy[curr];
+        lazy[2*curr + 1]+= lazy[curr];
+        lazy[curr] = 0;
         return;
     }
 
-    int mid=(l+r)/2;
-    update_min(ul,ur,value,2*curr,l,mid);
-    update_min(ul,ur,value,2*curr+1,mid+1,r);
+    void update(int ul,int ur,int val,int l,int r,int curr=1){
+        if(r<l) return;
 
-    seg_tree_min_range_query[curr]=min(seg_tree_min_range_query[2*curr],seg_tree_min_range_query[2*curr+1]);
-}
+        if(lazy[curr]!=0) propogate_down(curr);
 
+        if(r<ul || l>ur) return;
 
-int query_min(int ql,int qr,int curr=1,int l=0,int r=n-1){
-    if(r<l) return INT_MAX;
-    if(ql>r||qr<l) return INT_MAX;
+        if(l>=ul && r<=ur){
+            seg[curr].first+=val;
+            lazy[2*curr]+=val; lazy[2*curr+1]+=val;
+            return;
+        }
 
-    if(lazy_min_range_query[curr]){
-        propogate_to_child_min_query(curr,lazy_min_range_query[curr]);
-    }
-
-    if(ql<=l&&qr>=r) {
-        return seg_tree_min_range_query[curr];
-    }
-    return min(query_min(ql,qr,2*curr,l,(l+r)/2),query_min(ql,qr,2*curr+1,(l+r)/2+1,r));
-}
-
-
-
-
-
-int seg_tree_max_range_query[4*MAXN];
-int lazy_max_range_query[4*MAXN];
-
-
-void propogate_to_child_max_query(int curr,int value){
-    seg_tree_max_range_query[curr]+=value;
-    lazy_max_range_query[2*curr]+=value;
-    lazy_max_range_query[2*curr+1]+=value;
-    lazy_max_range_query[curr]=0;
-}
-
-void update_max(int ul,int ur,int value,int curr=1,int l=0,int r=n-1){
-
-    if(r<l) return ;
-    if(ul>r||ur<l) return ;
-
-    if(lazy_max_range_query[curr]){
-        propogate_to_child_max_query(curr,lazy_max_range_query[curr]);
-    }
-
-    if(l>=ul&&r<=ur){
-        seg_tree_max_range_query[curr]+=value;
-        lazy_max_range_query[2*curr]+=value;
-        lazy_max_range_query[2*curr+1]+=value;
+        int mid = (l+r)/2;
+        update(ul,ur,val,l,mid,2*curr);
+        update(ul,ur,val,mid+1,r,2*curr+1);
+        seg[curr] = combine(seg[2*curr],seg[2*curr +1]);
         return;
     }
 
-    int mid=(l+r)/2;
-    update_max(ul,ur,value,2*curr,l,mid);
-    update_max(ul,ur,value,2*curr+1,mid+1,r);
+    pair<int,int> query(int ql,int qr,int l,int r,int curr=1){
 
-    seg_tree_max_range_query[curr]=max(seg_tree_max_range_query[2*curr],seg_tree_max_range_query[2*curr+1]);
-}
+        if(r<l) return {2*inf,2*inf};
 
+        if(lazy[curr]) propogate_down(curr);
 
-int query_max(int ql,int qr,int curr=1,int l=0,int r=n-1){
-    if(r<l) return INT_MIN;
-    if(ql>r||qr<l) return INT_MIN;
+        if(r<ql || l>qr) return {2*inf,2*inf};
 
-    if(lazy_max_range_query[curr]){
-        propogate_to_child_max_query(curr,lazy_max_range_query[curr]);
+        if(l>=ql&&r<=qr){
+            return seg[curr];
+        }
+        int mid = (l+r)/2;
+        return combine(query(ql,qr,l,mid,2*curr),query(ql,qr,mid+1,r,2*curr+1));
+
     }
 
-    if(ql<=l&&qr>=r) {
-        return seg_tree_max_range_query[curr];
-    }
-    return max(query_max(ql,qr,2*curr,l,(l+r)/2),query_max(ql,qr,2*curr+1,(l+r)/2+1,r));
-}
-
-
-
-
-
-int32_t main(){
-    int n,m;cin>>n>>m;
-    vector<int> v(n);
-    for(auto &i : v) cin>>i;
-    for(int i=1;i<n;i++) v[i]+=v[i-1];
-    for(int i=0;i<n;i++){
-        update_max(i,i,v[i]);
-        update_min(i,i,v[i]);
-    }
-    while(1){
-        int x,y;
-        cin>>x>>y;
-        cout<<query_max(x,y)<<"\n";
-        cout<<query_min(x,y)<<"\n";
-    }
-    return 0;
-}
+};
